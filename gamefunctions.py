f63a025b5a77455cd30b8f66c99c6496c7552a61
jarredlost <lostotaku@outlook.com>
2020-03-07 12:14:21 +1100

    moved main game loop to game functions

--- gamefunctions.py
+++ gamefunctions.py
@@ -85,3 +85,49 @@ def checkforwin(field):
     if (emptyspances == 0 and winingplayer > 0):
         winingplayer = -1
     return winingplayer
+
+def startgame():
+    #array for gamefield
+    field = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+
+    #select the starting player
+    startingplayer = selectrandomplayer()
+    print("The starting player is player " + str(startingplayer))
+
+    gamewon = False
+    curplayer = startingplayer
+    winingplayer = 0
+    while not gamewon :
+        #inital draw the game field 
+        drawscreen(field)
+    
+        #get player input, update field array 
+        print("Player " +  str(curplayer) + " select a square")
+    
+        #get the playerinput
+        playerinputs = getplayerinput(field)
+        #seperate returned values into x and y
+        playerinputy = playerinputs[1]
+        playerinputx = playerinputs[0] 
+                
+                
+        field[playerinputy][playerinputx] = curplayer
+    
+        #alternate the players
+        if (curplayer == 1):
+            curplayer = 2
+        else:
+            curplayer = 1
+        #check for a winning player, if so trigger the end game
+        winingplayer = checkforwin(field)
+    
+        if (winingplayer != 0):
+            gamewon = True
+            if (winingplayer > 0 ):
+                print("the game has been won by player" + str(winingplayer))
+            elif (winingplayer == -1):
+                print("the game has run out of spaces, the game is a draw")
--- main.py
+++ main.py
@@ -5,50 +5,5 @@ from random import randint
 #my libeary 
 from gamefunctions import *
 
-#array for gamefield
-field = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-]
-
-
-    
-startingplayer = selectrandomplayer()
-print("The starting player is player " + str(startingplayer))
-
-gamewon = False
-curplayer = startingplayer
-winingplayer = 0
-while not gamewon :
-    #inital draw the game field 
-    drawscreen(field)
-    
-    #get player input, update field array 
-    print("Player " +  str(curplayer) + " select a square")
-    
-    #get the playerinput
-    playerinputs = getplayerinput(field)
-    #seperate returned values into x and y
-    playerinputy = playerinputs[1]
-    playerinputx = playerinputs[0] 
-                
-                
-    field[playerinputy][playerinputx] = curplayer
-    
-    #alternate the players
-    if (curplayer == 1):
-        curplayer = 2
-    else:
-        curplayer = 1
-    #check for a winning player, if so trigger the end game
-    winingplayer = checkforwin(field)
-    
-    if (winingplayer != 0):
-        gamewon = True
-        if (winingplayer > 0 ):
-            print("the game has been won by player" + str(winingplayer))
-        elif (winingplayer == -1):
-            print("the game has run out of spaces, the game is a draw")
-
+startgame()
                             
