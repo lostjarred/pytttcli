5e4c9307cd48d9895a70638274d6b2a189aaba47
jarredlost <lostotaku@outlook.com>
2020-03-07 12:27:04 +1100

    Added program Main Loop

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from random import randint
 
 #my libeary 
 from gamefunctions import *
+quit=False
 
-startgame()
+while (quit == False):
+    startgame()
+    userinput = input("Would you like to quit Y/N")
+    if (userinput == "y" or userinput == "Y"):
+        quit = True
+    elif (userinput == "n" or userinput == "N"):
+        startgame()
+    else:
+        print("Invaild input, plese answer again") 
+    
+    
                             
